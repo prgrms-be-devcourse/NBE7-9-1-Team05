64c8275530d99c5e060a0dd46b76e51151252b76
Ho-JunYu <hojoon1011@gmail.com>
2025-09-23 16:08:42 +0900

    feat: Product 엔티티 생성 - id, name, price, description 필드 포함

---
+++ src/main/java/demo/cafemenu/domain/product/Product.java
@@ -0,0 +1,37 @@
+package demo.cafemenu.domain.product;
+
+import demo.cafemenu.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Product extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 120)
+  private String name;
+
+  @Column(nullable = false)
+  private Integer price;
+
+  @Column(length = 1000)
+  private String description;
+
+}

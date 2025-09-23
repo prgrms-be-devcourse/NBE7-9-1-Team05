b0f23d08076b29cf2da571091d830d8491f696b7
HongRae-Kim <ghdfo7882@gmail.com>
2025-09-23 12:25:59 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/demo/cafemenu/CafeMenuApplication.java
@@ -0,0 +1,13 @@
+package demo.cafemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/cafemenu/CafeMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.cafemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

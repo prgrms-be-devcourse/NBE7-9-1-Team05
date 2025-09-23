c49f45a5cc612763e84f7683f18653c41b2f4db2
Ho-JunYu <hojoon1011@gmail.com>
2025-09-23 15:37:13 +0900

    feat: domain 패키지 및 BaseTimeEntity 생성
    - 엔티티 공통 시간 필드 관리를 위한 추상 클래스 추가
    - JPA Auditing 기능을 활용하여 createdAt, updatedAt 자동 관리

---
+++ src/main/java/demo/cafemenu/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package demo.cafemenu.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDate;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+  @CreatedDate
+  @Column(updatable = false)
+  protected LocalDate createdAt;
+
+  @LastModifiedDate
+  protected LocalDate updatedAt;
+
+}

47ae3840f0737c02341113d17b00ba138dc427e9
Ho-JunYu <hojoon1011@gmail.com>
2025-09-23 15:38:52 +0900

    chore: @EnableJpaAuditing 설정하여 엔티티 시간 자동 관리 활성화

--- src/main/java/demo/cafemenu/CafeMenuApplication.java
+++ src/main/java/demo/cafemenu/CafeMenuApplication.java
@@ -2,7 +2,9 @@ package demo.cafemenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CafeMenuApplication {
 

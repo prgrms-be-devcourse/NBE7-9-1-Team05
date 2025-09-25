60328c84e2adafc43c659b66af377fb7015694ce
Ho-JunYu <hojoon1011@gmail.com>
2025-09-26 01:51:45 +0900

    feat: UserController 클래스 생성 및 회원가입 API 엔드포인트 구현

--- backend/src/main/java/demo/cafemenu/domain/user/controller/UserController.java
+++ backend/src/main/java/demo/cafemenu/domain/user/controller/UserController.java
@@ -1,5 +1,28 @@
 package demo.cafemenu.domain.user.controller;
 
+import demo.cafemenu.domain.user.dto.SignupRequest;
+import demo.cafemenu.domain.user.dto.SignupResponse;
+import demo.cafemenu.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping("/signup")
+  @ResponseStatus(HttpStatus.CREATED)
+  public SignupResponse signup(@Validated @RequestBody SignupRequest request) {
+    return userService.signup(request);
+  }
 
 }

eb681c497392283511931efca111b441c02523f0
Ho-JunYu <hojoon1011@gmail.com>
2025-09-26 01:42:02 +0900

    feat: SignupRequest record 생성 및 필드에 Java Bean Validation 어노테이션 적용

---
+++ backend/src/main/java/demo/cafemenu/domain/user/dto/SignupRequest.java
@@ -0,0 +1,23 @@
+package demo.cafemenu.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest (
+
+    @Email(message = "유효한 이메일 주소를 입력해 주세요.")
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    String email,
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$])[A-Za-z\\d!@#$]{8,}$",
+        message = "비밀번호는 최소 8자 이상이며, 영문, 숫자, 특수문자(!,@,#,$)를 각각 하나 이상 포함해야 합니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    String password,
+
+    @Size(min = 2, max = 20)
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
+    String name
+
+) {}

75a8c2f86d15fbafcaa8aa8e8a41fad6f305ec7f
Ho-JunYu <hojoon1011@gmail.com>
2025-09-25 16:52:58 +0900

    refactor: 타임리프 MVC 레이어 삭제 및 REST 전환 준비

--- backend/src/main/java/demo/cafemenu/domain/customer/controller/CustomerController.java
+++ backend/src/main/java/demo/cafemenu/domain/customer/controller/CustomerController.java
@@ -1,25 +1,5 @@
 package demo.cafemenu.domain.customer.controller;
-import demo.cafemenu.domain.product.entity.Product;
-import demo.cafemenu.domain.product.service.ProductService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
-@Controller("productMenuController")
 public class CustomerController {
-    private final ProductService productService;
 
-    public CustomerController(ProductService productService){
-        this.productService = productService;
-    }
-
-    // cafemenu.controller로 옮길 시, 해당 코드 옮기기
-    // 전체 조회
-//    @GetMapping("/api/beans")
-//    @ResponseBody
-//    public List<Product> getProducts(){
-//        return productService.findAll();
-//    }
 }
--- backend/src/main/java/demo/cafemenu/domain/customer/service/CustomertService.java
+++ backend/src/main/java/demo/cafemenu/domain/customer/service/CustomertService.java
@@ -1,22 +1,5 @@
 package demo.cafemenu.domain.customer.service;
 
-import demo.cafemenu.domain.product.entity.Product;
-import demo.cafemenu.domain.product.repository.ProductRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service("productMenuServ")
 public class CustomertService {
 
-    private final ProductRepository productRepository;
-
-    public CustomertService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    // cafemenu.service로 옮길 시, 해당 코드 옮기기
-    public List<Product> findAll(){
-        return productRepository.findAll();
-    }
 }

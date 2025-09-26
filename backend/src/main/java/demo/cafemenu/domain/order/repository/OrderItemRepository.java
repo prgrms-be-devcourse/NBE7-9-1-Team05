0f67607dbcbf0c9cca22bf0094c0a036b2bdb8c7
nokwontae <acer2level@naver.com>
2025-09-27 00:32:26 +0900

    chore: Order 관련 파일 이름 변경

--- backend/src/main/java/demo/cafemenu/domain/order/repository/OrderItemRepository.java
+++ backend/src/main/java/demo/cafemenu/domain/order/repository/OrderItemRepository.java
@@ -1,13 +1,7 @@
 package demo.cafemenu.domain.order.repository;
 
 import demo.cafemenu.domain.order.entity.OrderItem;
-import demo.cafemenu.domain.order.entity.OrderStatus;
-import demo.cafemenu.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-    List<OrderItem> findByOrder_UserAndOrder_Status(User user, OrderStatus orderStatus);
 }

40dffa75e65ec4b83f62b980e5fff76d99543e8b
Ho-JunYu <hojoon1011@gmail.com>
2025-09-27 00:38:26 +0900

    chore: ProductRepository에 @Repository 어노테이션 추가

--- backend/src/main/java/demo/cafemenu/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/demo/cafemenu/domain/product/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package demo.cafemenu.domain.product.repository;
 
 import demo.cafemenu.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }

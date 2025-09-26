fc17a9162d2e2a0494cd12bde5c1123bb15a3969
Ho-JunYu <hojoon1011@gmail.com>
2025-09-26 15:42:17 +0900

    feat: SecurityConfig에 BCryptPasswordEncoder 빈 등록 메서드 추가

--- backend/src/main/java/demo/cafemenu/global/config/SecurityConfig.java
+++ backend/src/main/java/demo/cafemenu/global/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -64,5 +66,10 @@ public class SecurityConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration conf) throws Exception {
     return conf.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }

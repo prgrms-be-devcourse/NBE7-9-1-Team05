7a6203f75143907771b74e5d95aeb47fe637c178
nokwontae <acer2level@naver.com>
2025-09-26 14:06:04 +0900

    style: orderItem 관련 패키지 구조 제작

---
+++ backend/src/main/java/demo/cafemenu/domain/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package demo.cafemenu.domain.order.repository;
+
+import demo.cafemenu.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ backend/src/main/java/demo/cafemenu/domain/order/service/OrderItemService.java
@@ -0,0 +1,4 @@
+package demo.cafemenu.domain.order.service;
+
+public class OrderItemService {
+}

245a5d0a58425bc49aa098b253c7fdc2061bbe07
Ho-JunYu <hojoon1011@gmail.com>
2025-09-26 16:17:05 +0900

    refactor: UserDetailsServiceImpl에서 내장 User 대신 UserDetailsImpl 커스텀 구현체 반환으로 변경

--- backend/src/main/java/demo/cafemenu/global/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/demo/cafemenu/global/security/UserDetailsServiceImpl.java
@@ -2,9 +2,7 @@ package demo.cafemenu.global.security;
 
 import demo.cafemenu.domain.user.entity.User;
 import demo.cafemenu.domain.user.reposiitory.UserRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,11 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     User u = userRepository.findByEmail(email)
         .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
-    return new org.springframework.security.core.userdetails.User(
-        u.getEmail(),
-        u.getPassword(),
-        List.of(new SimpleGrantedAuthority(u.getRole().name()))
-    );
+    return new UserDetailsImpl(u.getId(), u.getEmail(), u.getPassword(), u.getRole());
   }
 
 }

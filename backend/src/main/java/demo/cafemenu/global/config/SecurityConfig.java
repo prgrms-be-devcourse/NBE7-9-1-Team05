55de45c30e9ffdaa02eecad5d24ccebd6ab1215b
Ho-JunYu <hojoon1011@gmail.com>
2025-09-26 15:41:20 +0900

    feat: SecurityConfig에 AuthenticationManager Bean 등록 메서드 추가

--- backend/src/main/java/demo/cafemenu/global/config/SecurityConfig.java
+++ backend/src/main/java/demo/cafemenu/global/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -59,4 +61,8 @@ public class SecurityConfig {
     return http.build();
   }
 
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration conf) throws Exception {
+    return conf.getAuthenticationManager();
+  }
 }

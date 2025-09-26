79878fd1afef1eef00958c3015fb892f0a0022ce
yhwHUB <yanghw1902@gmail.com>
2025-09-26 16:46:40 +0900

    refactor : @생성자 제거, @RequireArgsConstructor 추가

--- backend/src/main/java/demo/cafemenu/domain/product/controller/ProductController.java
+++ backend/src/main/java/demo/cafemenu/domain/product/controller/ProductController.java
@@ -2,7 +2,7 @@ package demo.cafemenu.domain.product.controller;
 
 import demo.cafemenu.domain.product.entity.Product;
 import demo.cafemenu.domain.product.service.ProductService;
-import org.antlr.v4.runtime.tree.pattern.ParseTreePattern;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping("/beans")
     public List<Product> getProduct(){

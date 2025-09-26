35378c5177f2201188449ee90f591dd673ca0078
yhwHUB <yanghw1902@gmail.com>
2025-09-26 23:22:07 +0900

    chore : UserService 사용하지  않는 선언 삭제

--- backend/src/main/java/demo/cafemenu/domain/user/service/UserService.java
+++ backend/src/main/java/demo/cafemenu/domain/user/service/UserService.java
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
   private final UserRepository userRepository;
-  private final ProductRepository productRepotiroty;
 
 
   public SignupResponse signup(SignupRequest request) {

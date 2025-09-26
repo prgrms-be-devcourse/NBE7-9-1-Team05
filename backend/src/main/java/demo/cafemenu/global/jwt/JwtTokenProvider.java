91056df0eb55b6e1a41688a05b8f1cb41f9c5ce4
Ho-JunYu <hojoon1011@gmail.com>
2025-09-26 14:23:06 +0900

    feat: JwtTokenProvider에 JWT 토큰 기반 Authentication 객체 생성 메서드 추가

--- backend/src/main/java/demo/cafemenu/global/jwt/JwtTokenProvider.java
+++ backend/src/main/java/demo/cafemenu/global/jwt/JwtTokenProvider.java
@@ -1,6 +1,9 @@
 package demo.cafemenu.global.jwt;
 
+import demo.cafemenu.domain.user.entity.Role;
 import demo.cafemenu.domain.user.entity.User;
+import demo.cafemenu.global.security.UserDetailsImpl;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -10,6 +13,8 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -45,5 +50,20 @@ public class JwtTokenProvider {
   // JWT 토큰 유효성 검사
   public void validateToken(String token) throws JwtException {
     Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+  }
+
+  // 인증 객체 생성
+  public Authentication getAuthentication(String token) {
+    Claims claims = Jwts.parserBuilder().setSigningKey(key).build()
+        .parseClaimsJws(token).getBody();
+
+    String email = claims.getSubject();
+    Long id = claims.get("id", Number.class).longValue();
+    Role role = Role.valueOf(claims.get("role", String.class));
+
+    UserDetailsImpl userDetails = new UserDetailsImpl(id, email, null, role);
+    return new UsernamePasswordAuthenticationToken(
+        userDetails, null, userDetails.getAuthorities()
+    );
   }
 }

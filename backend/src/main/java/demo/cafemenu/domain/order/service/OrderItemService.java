14e4cfe453bcbffb01e5722a761da7b4c7f00c87
nokwontae <acer2level@naver.com>
2025-09-26 15:23:15 +0900

    feat: OrderItemService 생성 및 getOrderItemByUserId 구현

--- backend/src/main/java/demo/cafemenu/domain/order/service/OrderItemService.java
+++ backend/src/main/java/demo/cafemenu/domain/order/service/OrderItemService.java
@@ -1,4 +1,51 @@
 package demo.cafemenu.domain.order.service;
 
+import demo.cafemenu.domain.order.dto.OrderItemDto;
+import demo.cafemenu.domain.order.entity.OrderItem;
+import demo.cafemenu.domain.order.entity.OrderStatus;
+import demo.cafemenu.domain.order.repository.OrderItemRepository;
+import demo.cafemenu.domain.order.repository.OrderRepository;
+import demo.cafemenu.domain.product.repository.ProductRepository;
+import demo.cafemenu.domain.user.entity.User;
+import demo.cafemenu.domain.user.reposiitory.UserRepository;
+import demo.cafemenu.global.exception.BusinessException;
+import demo.cafemenu.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class OrderItemService {
+
+    private final OrderItemRepository orderItemRepository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    /* 주문 내역 조회
+    - userId 기준으로 주문 목록 가져오기(userDetail 도입 시 변경 예정)
+    - PAID 상태만
+     */
+    @Transactional
+    public List<OrderItemDto> getOrderItemByUserId(Long userId) {
+        User user = userRepository.findById(userId).get();
+
+        List<OrderItem> paidItems = orderItemRepository
+                .findByOrder_UserAndOrder_Status(user, OrderStatus.PAID);
+        if (paidItems.isEmpty()) {
+            throw new BusinessException(ErrorCode.PAID_ORDERS_NOT_FOUND);
+        }
+
+        return paidItems.stream()
+                .map(item -> new OrderItemDto(
+                        item.getProductId(),
+                        item.getQuantity(),
+                        item.getLineAmount(),
+                        item.getOrder().getBatchDate()
+                ))
+                .toList();
+    }
 }

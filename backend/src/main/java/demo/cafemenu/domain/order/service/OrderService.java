041794abb42e9e01e5cb412389b2ac97aab41069
nokwontae <acer2level@naver.com>
2025-09-27 00:27:17 +0900

    feat: OrderService 생성 및 getPaidOrderByUserId

---
+++ backend/src/main/java/demo/cafemenu/domain/order/service/OrderService.java
@@ -0,0 +1,52 @@
+package demo.cafemenu.domain.order.service;
+
+import demo.cafemenu.domain.order.dto.OrderDto;
+import demo.cafemenu.domain.order.entity.Order;
+import demo.cafemenu.domain.order.entity.OrderStatus;
+import demo.cafemenu.domain.order.repository.OrderItemRepository;
+import demo.cafemenu.domain.order.repository.OrderRepository;
+import demo.cafemenu.domain.product.repository.ProductRepository;
+import demo.cafemenu.domain.user.entity.User;
+import demo.cafemenu.domain.user.reposiitory.UserRepository;
+import demo.cafemenu.global.exception.BusinessException;
+import demo.cafemenu.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderItemRepository orderItemRepository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    /* 주문 내역 조회
+    - userId 기준으로 주문 목록 가져오기(추후 인증기반 조회로 변경 예정)
+    - PAID 상태만
+     */
+    @Transactional
+    public List<OrderDto> getPaidOrderByUserId(Long userId) {
+        User user = userRepository.findById(userId).get();
+
+        List<Order> paidOrder = orderRepository
+                .findByUserAndStatus(user, OrderStatus.PAID);
+        if (paidOrder.isEmpty()) {
+            throw new BusinessException(ErrorCode.PAID_ORDERS_NOT_FOUND);
+        }
+
+        return paidOrder.stream()
+                .map(item -> new OrderDto(
+                        item.getId(),
+                        item.getUser().getEmail(),
+                        item.getBatchDate(),
+                        item.getTotalAmount(),
+                        item.getStatus()
+                ))
+                .toList();
+    }
+}

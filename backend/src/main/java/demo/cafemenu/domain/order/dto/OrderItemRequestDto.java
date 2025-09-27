8499d00339c4c6f12f88b71c448f58518749e6b9
nokwontae <acer2level@naver.com>
2025-09-27 12:48:16 +0900

    feat: OrderItemRequestDto 생성

---
+++ backend/src/main/java/demo/cafemenu/domain/order/dto/OrderItemRequestDto.java
@@ -0,0 +1,14 @@
+package demo.cafemenu.domain.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemRequestDto {
+    Long productId;
+}

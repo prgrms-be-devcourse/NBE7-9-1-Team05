f62cb696050f4b1b1032d2bcb2c2cb2c41888a55
nokwontae <acer2level@naver.com>
2025-09-27 20:46:38 +0900

    refactor: findByUserAndStatus -> findAllByUserAndStatus 오타 수정

--- backend/src/main/java/demo/cafemenu/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/demo/cafemenu/domain/order/repository/OrderRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserAndStatus(User user, OrderStatus status);
+    List<Order> findAllByUserAndStatus(User user, OrderStatus status);
 }

41015de917cb0298ec65337009356163cb34bb49
Ho-JunYu <hojoon1011@gmail.com>
2025-09-27 20:40:08 +0900

    refactor: getProduct 메서드 반환 타입 변경 및 서비스 메서드 호출로 리팩토링

--- backend/src/main/java/demo/cafemenu/domain/product/controller/ProductController.java
+++ backend/src/main/java/demo/cafemenu/domain/product/controller/ProductController.java
@@ -1,13 +1,12 @@
 package demo.cafemenu.domain.product.controller;
 
-import demo.cafemenu.domain.product.entity.Product;
+import demo.cafemenu.domain.product.dto.ProductResponse;
 import demo.cafemenu.domain.product.service.ProductService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,7 +16,7 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/beans")
-    public List<Product> getProduct(){
-        return productService.findAll();
+    public List<ProductResponse> getProduct() {
+        return productService.getAll();
     }
 }

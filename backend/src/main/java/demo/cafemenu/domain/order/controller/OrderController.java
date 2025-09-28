61289e5ff2532ac421d2c4b0de86fa770b574477
nokwontae <acer2level@naver.com>
2025-09-28 16:12:43 +0900

    fix: OrderController에 import ... .UserDetailsImpl로 다시 변경

--- backend/src/main/java/demo/cafemenu/domain/order/controller/OrderController.java
+++ backend/src/main/java/demo/cafemenu/domain/order/controller/OrderController.java
@@ -3,7 +3,7 @@ package demo.cafemenu.domain.order.controller;
 import demo.cafemenu.domain.order.dto.CheckoutRequest;
 import demo.cafemenu.domain.order.dto.OrderDto;
 import demo.cafemenu.domain.order.service.OrderService;
-import demo.cafemenu.global.security.*;
+import demo.cafemenu.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;

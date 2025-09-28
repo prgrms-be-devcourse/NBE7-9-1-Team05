952cfacd4b892af7ad5b2c5aec49242f0a6ecd91
yhwHUB <yanghw1902@gmail.com>
2025-09-28 13:49:56 +0900

    chore : ãpull ì진 행 후 코드 정리 커밋

--- backend/src/main/java/demo/cafemenu/domain/order/controller/OrderController.java
+++ backend/src/main/java/demo/cafemenu/domain/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package demo.cafemenu.domain.order.controller;
 
+import demo.cafemenu.domain.order.dto.CheckoutRequest;
 import demo.cafemenu.domain.order.dto.OrderDto;
 import demo.cafemenu.domain.order.service.OrderService;
 import demo.cafemenu.global.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+
 
     // 주문내역 조회
     @GetMapping("/order")
@@ -43,8 +47,16 @@ public class OrderController {
     @PostMapping("/orders/checkout")
     @ResponseStatus(HttpStatus.OK)
     public void checkoutAll(@AuthenticationPrincipal UserDetailsImpl principal,
-        @Valid @RequestBody CheckoutRequest request) {
+                            @Valid @RequestBody CheckoutRequest request) {
         orderService.checkoutAllPending(principal.getId(), request);
+    }
+
+    // 장바구니 조회
+    @GetMapping("/order/cart")
+    public List<OrderDto> getPendingOrdersByUser(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getId();
+        return orderService.getPendingOrdersByUser(userId);
+
     }
 }
 
